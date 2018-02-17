42379b3f6a7f43af592ee1f00ce8855cc55beeef
patrickatcw <pj8392@yahoo.com>
2018-02-17 15:41:11 -0500

    added directions for program in Main

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,3 +1,16 @@
+/*Directions
+-create a new class for a bank account
+-create fields for the account number, balance, customer name, email,
+and phone number
+-create two additional methods;
+1) to allow the customer to deposit funds (this should increment the balance field)
+2) to allow the customer to withdraw funds (this should deduct from
+the balance field, but not allow the withdrawal to complete if there
+are insufficient funds)
+-you will want to create various code in the Main class to confirm
+your code is working
+-add some println's in the 2 methods above as well*/
+
 package com.me;
 
 public class Main {
