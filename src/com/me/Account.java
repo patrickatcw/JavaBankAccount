bea921f579c58ae2b2079dfb097272715ab89296
patrickatcw <pj8392@yahoo.com>
2018-02-17 21:02:40 -0500

    fields and constructor created in Account class

--- src/com/me/Account.java
+++ src/com/me/Account.java
@@ -1,4 +1,19 @@
 package com.me;
 
+//Step 1, create a new class for a bank account
 public class Account {
+
+    //Step 2 create fields for the account number, balance, customer name, email, & phone number
+    private int accountNumber;
+    private double balance;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhoneNumber;
+
+    //Step 3 constructor with parameters
+    public Account(String accountNumber, double balance, String customerName, String customerEmail,
+                   String customerPhoneNumber) {
+
+    }
+
 }
--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,15 +1,13 @@
 /*Directions
--create a new class for a bank account
--create fields for the account number, balance, customer name, email,
-and phone number
--create two additional methods;
-1) to allow the customer to deposit funds (this should increment the balance field)
-2) to allow the customer to withdraw funds (this should deduct from
-the balance field, but not allow the withdrawal to complete if there
-are insufficient funds)
--you will want to create various code in the Main class to confirm
-your code is working
--add some println's in the 2 methods above as well*/
+1) create a new class for a bank account
+2) create fields for the account number, balance, customer name, email, & phone number
+3) make constructor with parameters
+4) create two additional methods;
+- one method to allow the customer to deposit funds (this should increment the balance field)
+- the second method to allow the customer to withdraw funds (this should deduct from
+the balance field, but not allow the withdrawal to complete if there are insufficient funds)
+4) create various code in the Main class to confirm your code is working
+5) add some println's in the 2 methods above as well*/
 
 package com.me;
 
